b055e072bf506e66a92de9af8c672b58d69fb45c
blato <blator02@gmail.com>
2025-05-19 16:07:35 +0200

    3.0

---
+++ ex9/README.md
@@ -0,0 +1,17 @@
+**Zadanie 9**
+
+ChatGPT bot. Należy rozszerzyć funkcjonalność wcześniej stworzonego bota. Do niego
+należy stworzyć aplikajcę frontendową, która połączy się z osobnym
+serwisem, który przeanalizuje tekst od użytkownika i prześle zapytanie
+do GPT, a następnie prześle odpowiedź do użytkownika. Cały projekt
+należy stworzyć w Pythonie.
+
+- :white_check_mark: 3.0 należy stworzyć po stronie serwerowej osobny serwis do łącznia z
+chatGPT do usługi chat
+- :x: 3.5 należy stworzyć interfejs frontowy dla użytkownika, który
+komunikuje się z serwisem; odpowiedzi powinny być wysyałen do
+frontendowego interfejsu
+- :x: 4.0 stworzyć listę 5 różnych otwarć oraz zamknięć rozmowy
+- :x: 4.5 filtrowanie po zagadnieniach związanych ze sklepem (np.
+ograniczenie się jedynie do ubrań oraz samego sklepu) do GPT
+- :x: 5.0 filtrowanie odpowiedzi po sentymencie
---
+++ ex9/server.py
@@ -0,0 +1,50 @@
+# from openai import AsyncOpenAI
+from groq import AsyncGroq
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from dotenv import load_dotenv
+
+# load environment variables from .env file (GROQ_API_KEY)
+load_dotenv()
+
+# define the data model for the user message and bot response
+class UserMessage(BaseModel):
+    message: str
+
+class BotResponse(BaseModel):
+    reply: str
+    error: Optional[str] = None
+
+# set up FastAPI app and Groq client 
+# (Groq client will automatically use the OPENAI_API_KEY from the environment)
+app = FastAPI()
+groq = AsyncGroq()
+
+# routes
+@app.post("/chat", response_model=BotResponse)
+async def chat(user_input: UserMessage):
+    user_message = user_input.message
+
+    if not user_message:
+        return BotResponse(reply="", error="ERROR: No message provided.")
+
+    chat_response = await get_response(user_message)
+
+    return BotResponse(reply=chat_response)
+
+# helper functions
+async def get_response(user_message: str) -> str:
+    try:
+        chat_completion = await groq.chat.completions.create(
+            # llama-3.3-70b-versatile
+            model="meta-llama/llama-4-maverick-17b-128e-instruct",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": user_message}
+            ]
+        )
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        print(f"Error calling API: {e}")
+        raise # re-raise to be handled by the route
